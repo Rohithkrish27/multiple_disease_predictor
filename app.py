d1c7c23e1f4f78c7e045d0aa6a5eb851097df5e6
Rohith Krishna <142262840+Rohithkrish27@users.noreply.github.com>
2024-07-21 02:03:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import pickle 
 import os
-import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title="Mulitple Disease Prediction",layout="wide", page_icon="👨‍🦰🤶")
